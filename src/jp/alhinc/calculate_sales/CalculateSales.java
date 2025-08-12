bc4eb49a5cbbd2209b8fe522ad6b2b596819f653
shiraiwa_kazuki <shiraiwa.kazuki@alhinc.jp>
2025-08-12 11:37:56 +0900

    コメント行のインデントを修正

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 public class CalculateSales {
-		//以下定数
+	//以下定数
 	// 支店定義ファイル名
 	private static final String FILE_NAME_BRANCH_LST = "branch.lst";
 
@@ -32,55 +32,62 @@ public class CalculateSales {
 	 * @param コマンドライン引数
 	 */
 	public static void main(String[] args) {
-			// 支店コードと支店名を保持するMap
+		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
-			// 支店コードと売上金額を保持するMap
+		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 
-			// 支店定義ファイル読み込み処理　※エラーが出たら実行しない
+		// 支店定義ファイル読み込み処理　※エラーが出たら実行しない
 		if(!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
 			return;
 		}
 
-			// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
-			//特定のフォルダーにあるファイルを配列として「File[] files」に保存する
-			//コマンドライン引数で指定しているので、args[0]でOK
+		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		//特定のフォルダーにあるファイルを配列として「File[] files」に保存する
+		//コマンドライン引数で指定しているので、args[0]でOK
 		File[] files = new File(args[0]).listFiles();
-			//この後に行うファイル名のチェックを通過したものを入れる「List<File>」を作成する
+		//この後に行うファイル名のチェックを通過したものを入れる「List<File>」を作成する
 		List<File> rcdFiles = new ArrayList<>();
 
-			//ファイル名のチェック機能（「数字8桁.rcd」になっているか）
-		for(int i = 0; i < files.length; i++) {			//filesは配列なので「.size」ではなく「.length」
-			if(files[i].getName().matches("^[0-9]{8}[.]rcd$" )) {		//正規表現式を参照
+		//ファイル名のチェック機能（「数字8桁.rcd」になっているか）
+		//filesは配列なので「.size」ではなく「.length」
+		for(int i = 0; i < files.length; i++) {
+			//正規表現式を参照
+			if(files[i].getName().matches("^[0-9]{8}[.]rcd$" )) {
 				rcdFiles.add(files[i]);
 			}
 		}
 
-			//BufferedReaderの初期化
+		//BufferedReaderの初期化
 		BufferedReader br = null;
 
-			//rcdFilesの要素の読込機能
-		for(int i = 0; i < rcdFiles.size(); i++) {		//rcdFilesはListなので「.length」ではなく「.size」
+		//rcdFilesの要素の読込機能
+		//rcdFilesはListなので「.length」ではなく「.size」
+		for(int i = 0; i < rcdFiles.size(); i++) {
 
 			try {
-				FileReader fr = new FileReader(rcdFiles.get(i));	//rcdFilesはリストなので.get()
-				br = new BufferedReader(fr);	//初期化時にBufferedReader brとしているのでここではbrのみ
+				//rcdFilesはリストなので「.get()」
+				FileReader fr = new FileReader(rcdFiles.get(i));
+				//初期化時にBufferedReader brとしているのでここではbrのみ
+				br = new BufferedReader(fr);
 
-					//FileReaderで読み取った内容を保存するための「List<String>」を作成
+				//FileReaderで読み取った内容を保存するための「List<String>」を作成
 				List<String> storeSale = new ArrayList<>();
-					//読み取った行の情報（文字列）を代入するための変数line
+				//読み取った行の情報（文字列）を代入するための変数line
 				String line;
-				while((line = br.readLine()) != null) {		//rcdFilesのすべての行をreadLineするまで繰り返し
-					storeSale.add(line);	//読み取った内容をstoreSale(List<String>)に追加していく
+				//rcdFilesのすべての行をreadLineするまで繰り返し
+				while((line = br.readLine()) != null) {
+					//読み取った内容をstoreSale(List<String>)に追加していく
+					storeSale.add(line);
 				}
-					//String型の売上金額をLong型に変換
+				//String型の売上金額をLong型に変換
 				long fileSale = Long.parseLong(storeSale.get(1));
 
-					//branchSalesに保持されている売上金額に合計する
-					//合計を保存するためのsaleAmountを作成し、計算式を代入
-					//branchSales.get(storeSale.get(0))は、storeSale.get(0)で得た支店コードをbranchSalesのkeyとして使用している
+				//branchSalesに保持されている売上金額に合計する
+				//合計を保存するためのsaleAmountを作成し、計算式を代入
+				//branchSales.get(storeSale.get(0))は、storeSale.get(0)で得た支店コードをbranchSalesのkeyとして使用している
 				Long saleAmount = branchSales.get(storeSale.get(0)) + fileSale;
-					//storeSaleで得た支店コード(key)に紐づくvalueを更新する
+				//storeSaleで得た支店コード(key)に紐づくvalueを更新する
 				branchSales.put(storeSale.get(0), saleAmount);
 
 			}catch(IOException e) {
@@ -100,8 +107,8 @@ public class CalculateSales {
 		}
 
 
-			// 支店別集計ファイル書き込み処理
-			//(ファイルパス、出力先のファイル名、出力元データ1、出力元データ2)
+		// 支店別集計ファイル書き込み処理
+		//(ファイルパス、出力先のファイル名、出力元データ1、出力元データ2)
 		if(!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
 			return;
 		}
@@ -126,10 +133,10 @@ public class CalculateSales {
 			br = new BufferedReader(fr);
 
 			String line;
-				// 一行ずつ読み込む
+			// 一行ずつ読み込む
 			while((line = br.readLine()) != null) {
-					// ※ここの読み込み処理を変更してください。(処理内容1-2)
-					//lineに代入される文字列を「,」で
+				// ※ここの読み込み処理を変更してください。(処理内容1-2)
+				//lineに代入される文字列を「,」で
 				String[] items = line.split(",");
 
 				branchNames.put(items[0], items[1]);
@@ -141,10 +148,10 @@ public class CalculateSales {
 			return false;
 
 		} finally {
-				// ファイルを開いている場合
+			// ファイルを開いている場合
 			if(br != null) {
 				try {
-						// ファイルを閉じる
+					// ファイルを閉じる
 					br.close();
 				} catch(IOException e) {
 					System.out.println(UNKNOWN_ERROR);
@@ -165,17 +172,17 @@ public class CalculateSales {
 	 * @return 書き込み可否
 	 */
 	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames, Map<String, Long> branchSales) {
-			// ※ここに書き込み処理を作成してください。(処理内容3-1)
+		// ※ここに書き込み処理を作成してください。(処理内容3-1)
 		BufferedWriter bw = null;
-			//keyの数の分だけ以下の処理を繰り返す
+		//keyの数の分だけ以下の処理を繰り返す
 		try {
 			File file = new File(path,fileName);
 			FileWriter fw = new FileWriter(file);
 			bw = new BufferedWriter(fw);
 
 			for(String key : branchNames.keySet()) {
-					//実際にファイルに出力する形式を設定
-					//支店コード、支店名、合計金額をカンマ区切りで出力、支店ごとに改行
+				//実際にファイルに出力する形式を設定
+				//支店コード、支店名、合計金額をカンマ区切りで出力、支店ごとに改行
 				bw.write(key + "," + branchNames.get(key) + "," + branchSales.get(key));
 					//改行
 				bw.newLine();
